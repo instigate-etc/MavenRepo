97ab238b9b50c038286a7bbf1c2ce0eea62c627c
Naira <pnairai28@mail.ru>
2014-07-02 20:48:18 +0400

    Adding mavenProject

---
+++ mavenproject/src/test/java/ru/apache_maven/AppTest.java
@@ -0,0 +1,45 @@
+package ru.apache_maven;
+
+import java.lang.Object;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.testng.annotations.*;
+import org.testng.Assert;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    private String var = System.getProperty("variable");
+    private String name="Good";
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        System.out.println("Hi, Happy day");
+        Assert.assertEquals(name,"Good","Fill file");
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
